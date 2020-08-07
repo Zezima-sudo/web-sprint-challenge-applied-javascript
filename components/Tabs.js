6038a50e240bbddd6ab9c8350d8005ba2bce2edf
Eric Rice <ericmichaelrice@protonmail.com>
2020-08-07 10:04:33 -0700

    cards almost done

--- components/Tabs.js
+++ components/Tabs.js
@@ -9,3 +9,31 @@
 //    <div class="tab">topic here</div>
 //
 // NOTE: you do _not_ need to install axios as it's included in the HTML via script element
+
+
+import axios from 'axios'
+
+const promise = axios.get('https://lambda-times-api.herokuapp.com/topics')
+console.log(promise)
+
+function tabs (obj) {
+    //objects
+   const tab = document.createElement('div')
+   tab.classList.add('tab')
+   tab.textContent = obj
+   return tab
+}   
+const entryPoint = document.querySelector('.topics')
+
+promise.then((response) => {
+    response.data.topics.forEach(i => {
+    const newTab = tabs(i)
+    entryPoint.appendChild(newTab)
+    })
+})
+
+.catch((errorResponse) => {
+    console.log('error!', errorResponse)
+  })
+
+
